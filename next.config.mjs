0da14fe1c6d18cc521ec29359c179ec9794a9741
spun <spun@users.noreply.github.com>
2026-08-01 08:40:31 +0000

    Enable experimental useTypeScriptCli - This is required to make nextjs work with TypeScript 7   https://nextjs.org/docs/app/api-reference/config/next-config-js/useTypeScriptCli

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const basePath = process.env.PUBLIC_BASE_PATH || undefined;
 
 const nextConfig = {
+  experimental: {
+    // Required to work with TypeScript 7
+    useTypeScriptCli: true,
+  },
   // Enable static export
   output: 'export',
   // GH pages path
